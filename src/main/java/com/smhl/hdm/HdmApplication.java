b4998a12a8bca4f93c9fb5194d46faecf27a300d
Stephen <s.prizio@hotmail.com>
2019-06-03 19:22:13 -0400

    adds todos

--- src/main/java/com/smhl/hdm/HdmApplication.java
+++ src/main/java/com/smhl/hdm/HdmApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HdmApplication {
 
+    //  TODO: implement logic to handle players/goalies with less than 2 games played. We don't want to consider them for the stats leaders
+
+    //  TODO: add league averages for /stats
+
     public static void main(String[] args) {
         SpringApplication.run(HdmApplication.class, args);
     }
